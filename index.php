6d269c188cb861ef0bc371f96e84e2c17d3289ac
root <anillakshya6@gmail.com>
2019-03-15 21:18:49 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <?php include("includes/head.php");?>
+  <body>
+    <?php include("includes/header.php");?>
+    <div class="card">
+    </div>
+    <div class="nav-card">
+    </div>
+  </body>
+</html>

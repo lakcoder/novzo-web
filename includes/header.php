b3dcada5815a13a87f38ab881837b81393601025
sainiyogesh21068 <sainiyogesh21068@gmail.com>
2019-03-16 01:44:53 +0530

    navbar

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,19 @@
 
   <body>
     <div class="nav-card">
+      <ul class="nav-list">
+        <li class="novzo-logo">
+          <a class="active" href="#home">
+            <img src="img\novzologo.png" alt="novzo-logo" class="novzo-img"/>
+          </a>
+        </li>
+        <li class="nav-search">
+          <form action="/action_page.php">
+            <input type="search" name="q" class="nav-input-search" placeholder="search"><br>
+            <!-- <input type="submit"> -->
+          </form>
+        </li>
+      </ul>
     </div>
   </body>
-
 </html>

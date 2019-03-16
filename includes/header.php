1964e014320b62719d8ee9bf9ad003096236844f
sainiyogesh21068 <sainiyogesh21068@gmail.com>
2019-03-16 12:40:10 +0530

    updates

--- includes/header.php
+++ includes/header.php
@@ -11,9 +11,39 @@
         </li>
         <li class="nav-search">
           <form action="/action_page.php">
-            <input type="search" name="q" class="nav-input-search" placeholder="search"><br>
+            <input type="search" name="q" class="nav-input-search" placeholder="search...">
             <!-- <input type="submit"> -->
           </form>
+        </li>
+        <!-- <li class="nav-user">
+          <a class="active" href="#home">
+            <ul class="nav-user-profile">
+              <li>
+                <img src="img\user-logo.png" alt="user-logo" class="user-img"/>
+              </li>
+              <li>
+                <p class="nav-user-text">Profile</p>
+              </li>
+            </ul>
+          </a>
+        </li> -->
+        <li class="nav-user">
+          <a class="active" href="#home">
+            <p class="nav-user-text">Profile</p>
+            <!-- <img src="img\novzologo.png" alt="novzo-logo" class="novzo-img"/> -->
+          </a>
+        </li>
+        <li class="nav-icon">
+          <a class="active" href="#home">
+            <p class="nav-user-text">Profile</p>
+            <!-- <img src="img\novzologo.png" alt="novzo-logo" class="novzo-img"/> -->
+          </a>
+        </li>
+        <li class="nav-icon">
+          <a class="active" href="#home">
+            <p class="nav-user-text">Profile</p>
+            <!-- <img src="img\novzologo.png" alt="novzo-logo" class="novzo-img"/> -->
+          </a>
         </li>
       </ul>
     </div>

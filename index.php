e2813cb91edbe02529eeb52fa0e76098ff10fb0a
root <anillakshya6@gmail.com>
2019-03-16 21:18:46 +0530

    index changes

--- index.php
+++ index.php
@@ -18,7 +18,12 @@
         </p>
       </div>
       <div class="novreads">
-        <p class="novreads-text"><b><b>nov</b></b>reads</p>
+        <p class="novreads-text"><b><b>nov</b></b>reads
+          <form class="new-post" action="/action_page.php">
+            <input type="search" name="q" class="nav-input-search" placeholder="search...">
+            <!-- <input type="submit"> -->
+          </form>
+        </p>
       </div>
       <div class="novpost">
         <div class="post-left">
@@ -29,7 +34,7 @@
           <p class="post-time">8 MAR</p>
         </div>
         <div class="post-middle">
-          <p class="user-name">Lakshya Shukla <b class="user-handle">@I_m_ceo</b> </p>
+          <p class="user-name">Lakshya Shukla <b class="user-handle">@lakcoder</b> </p>
           <p class="post-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
@@ -42,7 +47,7 @@
           <p class="post-time">8 MAR</p>
         </div>
         <div class="post-middle">
-          <p class="user-name">Lakshya Shukla <b class="user-handle">@I_m_ceo</b> </p>
+          <p class="user-name">Lakshya Shukla <b class="user-handle">@lakcoder</b> </p>
           <p class="post-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>

50533eef3ad12e321f9e0948f1c20561cf0d4c3d
root <anillakshya6@gmail.com>
2019-05-08 19:25:25 +0530

    test

--- dashboard.php
+++ dashboard.php
@@ -35,7 +35,7 @@
         </div>
         <div class="post-middle">
           <p class="user-name">Lakshya Shukla <b class="user-handle">@lakcoder</b> </p>
-          <p class="post-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p class="post-content">ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <p style="font-size:1.2rem;" class="ti-heart"></p>
         </div>
       </div>
